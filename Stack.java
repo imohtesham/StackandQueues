c31ec69ce4da5d8ea4d1d94d3bbbc4ccb5693123
imohtesham <ibraahimm92@gmail.com>
2022-10-30 21:27:16 +0530

    stack ability to peak and pop

--- Stack.java
+++ Stack.java
@@ -1,5 +1,6 @@
 import java.util.LinkedList;
 
+
 public class Stack<T> {
 
     private LinkedList<T> list;
@@ -12,6 +13,22 @@ public class Stack<T> {
         list.add(item);
     }
 
+    T peek() {
+        return list.getLast();
+    }
+
+    T pop() {
+        return list.removeLast();
+    }
+
+    int size() {
+        return list.size();
+    }
+
+    boolean isEmpty() {
+        return list.isEmpty();
+    }
+
     void show() {
         System.out.println(list);
     }
@@ -22,6 +39,17 @@ public class Stack<T> {
         nums.push(30);
         nums.push(56);
 
+        System.out.println("Elements in stack:");
         nums.show();
-    }
-}
+
+        int size = nums.size();
+        System.out.println("Size of stack: " + size);
+
+        while (! nums.isEmpty()) {
+            System.out.println(nums.peek());
+            nums.pop();
+        }
+
+        System.out.println("Elements in stack after popping:");
+        nums.show();
+    }}
